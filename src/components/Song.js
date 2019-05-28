db47b6bd5ab5d1fb4734f5e73e746a9fd4344d16
Rory MacGregor <you@example.com>
2019-05-29 00:28:20 +0100

    Added pictures and categories.

--- src/components/Song.js
+++ src/components/Song.js
@@ -4,9 +4,9 @@ const Song = (props) => {
     return (
         <div>
             <p>In at number {props.index}:</p>
-            <h2>Title: {props.entry["im:name"].label}</h2>
+            <h2>Title: {props.entry["im:name"].label} ({props.entry.category.attributes.term}) </h2>
             <h3>Artist: {props.entry["im:artist"].label}</h3>
-            <img alt="" src={props.entry["im:image"].label}/>
+            <img alt="" src={props.entry["im:image"][2].label}/>
         </div>
     )
 }
